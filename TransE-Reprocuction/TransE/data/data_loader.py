2a0e61aca59e0f2a65c4dd829372107941c7d696
fryptus <fryptus_Kang@163.com>
2024-10-20 19:46:24 +0800

    [fea] implement the test_dataloader method, no test yet

--- TransE-Reprocuction/TransE/data/data_loader.py
+++ TransE-Reprocuction/TransE/data/data_loader.py
@@ -1,4 +1,6 @@
 import codecs
+import json
+import numpy as np
 
 
 class DataLoader:
@@ -50,3 +52,39 @@ class DataLoader:
                 relation_set.add(r_)
 
         return entity_set, relation_set, triple_list
+
+    @staticmethod
+    def test_dataloader(entity_file, relation_file, test_file):
+        entity2id = {}
+        relation2id = {}
+        entity_dict = {}
+        relation_dict = {}
+        test_triple = []
+
+        with codecs.open(entity_file) as e_f:
+            lines = e_f.readlines()
+            for line in lines:
+                entity, embedding = line.strip().split('\t')
+                embedding = np.array(json.loads(embedding))
+                entity_dict[int(entity)] = embedding
+
+        with codecs.open(relation_file) as r_f:
+            lines = r_f.readlines()
+            for line in lines:
+                relation, embedding = line.strip().split('\t')
+                embedding = np.array(json.loads(embedding))
+                relation_dict[int(relation)] = embedding
+
+        with codecs.open(test_file) as t_f:
+            lines = t_f.readlines()
+            for line in lines:
+                triple = line.strip().split('\t')
+                if len(triple) != 3:
+                    continue
+                h_ = entity2id[triple[0]]
+                t_ = entity2id[triple[1]]
+                r_ = relation2id[triple[2]]
+
+                test_triple.append(tuple((h_, t_, r_)))
+
+        return entity_dict, relation_dict, test_triple
